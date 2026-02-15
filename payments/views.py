34f775c3d4b654cbe9ba09bd1e81316932eabf5e
MD ARAFAT HOSSEN <112323987+mdarafathossensojib@users.noreply.github.com>
2026-02-15 23:08:01 +0600

    Add API view for authenticated payment history

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from payments.models import Payment
+from payments.serializers import PaymentSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def my_payment_history(request):
+    payments = Payment.objects.filter(member=request.user).order_by('-created_at')
+    serializer = PaymentSerializer(payments, many=True)
+    return Response(serializer.data)
