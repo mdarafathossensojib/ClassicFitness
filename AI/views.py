2f34537c1d923a9d77eda688c128c613390fa1cc
ARAFAT <mdarafathossen.py@gmail.com>
2026-04-09 11:45:41 +0600

    AI Assistant Works

--- AI/views.py
+++ AI/views.py
@@ -11,7 +11,6 @@ import os
 api_key = os.environ.get('API_KEY') or config('API_KEY')
 client = genai.Client(api_key=api_key)
 
-
 class AIAssistantView(APIView):
     permission_classes = [IsAuthenticated]
 
@@ -29,25 +28,26 @@ class AIAssistantView(APIView):
         
         try:
             response = client.models.generate_content(
-                model='gemini-2.0-flash',
+                model='gemini-3-flash-preview', 
                 contents=prompt
             )
 
-            if not response or not hasattr(response, 'text'):
-                 return Response({'error': 'AI could not generate a response. Please try again.'}, status=500)
+            if response and response.text:
+                ai_text = response.text
+                
+                # Save to Database
+                AIPlan.objects.create(
+                    user=user,
+                    plan_type=plan_type,
+                    input_data=user_input,
+                    ai_response=ai_text
+                )
+                return Response({'response': ai_text}, status=status.HTTP_200_OK)
             
-            ai_text = response.text
+            return Response({'error': 'AI generated an empty response.'}, status=500)
             
-            # Save to Database
-            AIPlan.objects.create(
-                user=user,
-                plan_type=plan_type,
-                input_data=user_input,
-                ai_response=ai_text
-            )
-            
-            return Response({'response': ai_text}, status=status.HTTP_200_OK)
         except Exception as e:
+            print(f"DEBUG: Error calling Gemini API -> {str(e)}")
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
